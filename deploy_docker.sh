9714d1a562cf046544034e2deb939d4f77a24e81
qianjizhi <jizhi.qian@jimubox.com>
2019-03-21 19:33:13 +0800

    Update build path

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-cd $PROJ_PATH/iWeb
+cd $PROJ_PATH
 mvn clean install
 
 #准备ROOT.war包
-cd $PROJ_PATH/iWeb/target
+cd $PROJ_PATH/target
 mv iWeb.war ROOT.war
 
 #制作新的docker image - iweb
-cd $PROJ_PATH/iWeb
+cd $PROJ_PATH
 docker stop iWebObj
 docker rm iWebObj
 docker rmi iweb
